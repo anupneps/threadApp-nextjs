ce18f80f66948127b7c35150fd9e30924d9ed616
anupneps <anup.neps@outlook.com>
2023-09-28 23:48:02 +0300

    created User schema, user action and test mongoDb

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -20,6 +20,8 @@ import { Input } from "@/components/ui/input"
 import { Textarea } from '../ui/textarea'
 import { isBase64Image } from '@/lib/utils'
 import { useUploadThing } from '@/lib/uploadThing'
+import { updateUser } from '@/lib/actions/user.actions'
+import { usePathname, useRouter } from 'next/navigation'
 
 interface Props {
     user: {
@@ -36,6 +38,8 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
     const [files, setFiles] = useState<File[]>([])
     const { startUpload } = useUploadThing("media")
+    const router = useRouter();
+    const pathname = usePathname();
 
     const form = useForm({
         resolver: zodResolver(userValidation),
@@ -71,16 +75,29 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
         const blob = values.profile_photo;
         const hasImageChanged = isBase64Image(blob);
 
-        if(hasImageChanged){
+        if (hasImageChanged) {
             const imgRes = await startUpload(files)
 
-            if(imgRes && imgRes[0].url){
+            if (imgRes && imgRes[0].url) {
                 values.profile_photo = imgRes[0].url
             }
         }
 
-        //TODO : Update user profile 
+        //TODO : Update user profile :: calling updateUser() function from actions
+        await updateUser({
+            userId: user._id,
+            username: values.username,
+            name: values.name,
+            bio: values.bio,
+            image: values.profile_photo,
+            path: pathname
+        })
 
+        if (pathname=== '/profile/edit'){
+            router.back(); // go to the prevous page after editing 
+        } else {
+            router.push('/') // go from onboarding page to homepage
+        }
     }
 
     return (
--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -7,14 +7,19 @@ export const connectToDB = async () => {
 
     if(!process.env.MONGODB_URI) {
         console.log('MONGODB_URI not found');
+        return
     }
     if(isConnected) {
         console.log('=> Already connected to database');
+        return
     }
 
     try {
+        await mongoose.connect(process.env.MONGODB_URI)
+        isConnected=true    
+        console.log('connected to mongoDB')
         
     } catch (error) {
-        
+        console.log(error)
     }
 }
