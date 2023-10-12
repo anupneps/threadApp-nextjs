33f01f9cf83783903716ceb21b0fddddda5e705b
anupneps <anup.neps@outlook.com>
2023-10-12 23:51:12 +0300

    first post created and rendered in UI

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -32,3 +32,36 @@ export async function createThread({text, author, communityId, path}:Params){
         throw new Error(`Error creating thread : ${error}`)
     }
 }
+
+export async function fetchPosts(pageNumber=1, pageSize=20){
+    try {
+       connectToDB();
+       //Calculate the number of posts to skip 
+       const skipAmount = (pageNumber-1)*pageSize
+       
+       //Fetch the posts that have no parents (top-level threads...)
+       const postQuery = Thread.find({parentId: {$in:[null, undefined]}})
+        .sort({createdAt: 'desc'})
+        .skip(skipAmount)
+        .limit(pageSize)
+        .populate({path:'author', model:User})
+        .populate({
+            path:'children',
+            populate:{
+                path:'author',
+                model:User,
+                select:"_id name parentId image"
+            }
+        })
+
+        const totalPostsCount = await Thread.countDocuments({parentId:{$in:[null, undefined]}})
+        const posts = await postQuery.exec()
+
+        const isNext = totalPostsCount> skipAmount + posts.length
+
+        return {posts, isNext}
+
+    } catch (error:any) {
+        throw new Error(`Error fetching posts : ${error}`)
+    }
+}
